801dbd3f9b6715ec06ad7499d448678b824ab3a1
Ülkem Kasapoğlu <ulkemkasapoglu@Ulkem-MacBook-Pro.local>
2023-01-23 16:31:13 +0300

    ML Methods Added

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 WHERE PROGRAM BEGINS.
 """
 import sys
+import glob
+import numpy as np
+import itertools
+
 import helper.parser as cli_parser
 from helper.filebase import FileBase 
 from helper.anonymization import AnonymizationTypes
+from helper.ml_methods import ML_Methods
 from anonymization.k_anonymity import KAnonymity
 from anonymization.laplace_noise import LaplaceNoise
 
@@ -36,9 +41,30 @@ if __name__ == "__main__":
         sens_start, sens_end = dic["start_sens"], dic["end_sens"]
         num = dic["number"]
         laplace = LaplaceNoise(eps_start, eps_end, sens_start, sens_end, num)
-        anonymized_train_images = laplace.anonymize()
-        #laplace.save_anonymized(anonymized_train_images)
-        anonymized_test_images = laplace.anonymize(False)
-        #laplace.save_anonymized(anonymized_test_images,word="test")
+        if len(glob.glob("data/*")) == 0:
+            anonymized_train_images = laplace.anonymize()
+            #laplace.save_anonymized(anonymized_train_images)
+            anonymized_test_images = laplace.anonymize(False)
+            #laplace.save_anonymized(anonymized_test_images,word="test")
         
+        methods = dic["ml_methods"]
+        eps = laplace.eps_vals
+        sens = laplace.sensitivity_vals
+        combinations = list(itertools.product(eps,sens))
+        y_train = laplace.dataset[1]
+        y_test = laplace.dataset[5]
+        for e, s in combinations:
+            name_e = "%".join(str(e).split(".")) 
+            name_s = "%".join(str(s).split("."))
+            path = name_e + "_" + name_s + ".npy"
+            if len(glob.glob("data/train_" + path)) > 0 and len(glob.glob("data/test_" + path)) > 0:
+                x_train = np.load("data/train_" + path)[1:,:,:]
+                x_test = np.load("data/test_" + path)[1:,:,:]
+            ml_instance = ML_Methods([x_train,y_train,x_test,y_test],methods)
+            ml_instance.run()
+            ml_instance.generate_log(name_e,name_s)
+                
+                
+                
+         
         
